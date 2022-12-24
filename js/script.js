fc7aebf7f1d9f7d6e047a1ee861728351b69917c
Veronika_lushnei <103830139+Veronikalushnei@users.noreply.github.com>
2022-12-24 16:54:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$("#title-block").click(function(){
+    $('#sub-block').toggle(1000);
+});
+
+$("#title").click(function(){
+    $('#sub').toggle(1000);
+});
+
+$("#t-block").click(function(){
+    $('#s-block').toggle(1000);
+});
+
+$("#tex-block").click(function(){
+    $('#su-block').toggle(1000);
+});
+/*
+$("#title-block").click(function(){
+    $('#sub-block').hide();
+});
+
+$("#title-block").click(function(){
+    $('#sub-block').show();
+});
+*/
